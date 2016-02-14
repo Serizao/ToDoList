5c7d9cdd387d903b8c50e81782f975180f16f29d
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2016-02-14 03:12:35 +0100

    First commit

--- js/myscript.js
+++
@@ -1,9 +0,0 @@
-var tdbtn = document.getElementById('todo');
-tdbtn.addEventListener('click', function() {
-	document.location.href = 'index.php?tasksToDisplay=todo';
-});
-
-var dbtn = document.getElementById('done');
-dbtn.addEventListener('click', function() {
-	document.location.href = 'index.php?tasksToDisplay=done';
-});
